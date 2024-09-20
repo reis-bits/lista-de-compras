87eec4cba94be42a6affadd21d241b1f449ec495
reis-bits <eduardorprincepe007@gmail.com>
2024-09-20 14:25:29 -0300

    feat(app):Adicionada funcionalidade de botão e lista.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,64 @@
+const inputItem = document.getElementById('campo-item'); inputItem.value = "";
 const btnSalvarItem = document.getElementById('btn-salvar-item');
-const listaDeCompras = document.getElementById('lista-de-compras');
-listaDeCompras.innerHTML = "";
+const listaDeCompras = document.getElementById('lista-de-compras'); listaDeCompras.innerHTML = "";
+let itensNaLista = [];
+
 btnSalvarItem.addEventListener('click', adicionarItem);
 
 function adicionarItem() {
-    const campoItem = document.getElementById('campo-item');
-    let item = campoItem.value;
-    listaDeCompras.innerHTML += `
-                <li id="lista-item">
+    let item = inputItem.value;
+
+    if(itensNaLista.includes(item)) {
+        alert('Você já colocou esse item!');
+    } else {
+        itensNaLista.push(item);
+        listaDeCompras.innerHTML += `
+                <li id="item-${itensNaLista.indexOf(item)}" class="lista-item">
                     <div class="lista-item-container">
                         <div>
                             <div class="container-checkbox">
-                                <label for="checkbox-1">
-                                    <input type="checkbox" class="input-checkbox" id="checkbox" />
-                                    <div class="checkbox-customizado"></div>
+                                <label for="checkbox-${itensNaLista.indexOf(item)}">
+                                    <input type="checkbox" class="input-checkbox"  />
+                                    <div id="checkbox-${itensNaLista.indexOf(item)}" class="checkbox-customizado" onclick="checkbox(${itensNaLista.indexOf(item)})"></div>
                                 </label>
                             </div>
-                            <p> ${item} </p>
+                            <p id="nome-item-${itensNaLista.indexOf(item)}">${item}</p>
                         </div>
 
                         <div>
-                            <button class="item-lista-button">
-                                <img src="./img/delete.svg" alt="remover">
+                            <button id="delete-${itensNaLista.indexOf(item)}" class="item-lista-button" onclick="deletar(${itensNaLista.indexOf(item)})" >
+                                <img src="./img/delete.svg"></img>
                             </button>
-                            <button class="item-lista-button">
-                                <img src="./img/edit.svg" alt="editar">
+                            <button id="edit-${itensNaLista.indexOf(item)}" class="item-lista-button" onclick="editar(${itensNaLista.indexOf(item)})">
+                                <img src="./img/edit.svg"></img>
                             </button>
                         </div>
                     </div>
-                    <p class="texto-data">Segunda-feira (31/10/2022) às 08:30</p>
+                    <p class="texto-data"></p>
                 </li>
-    `;
+    `
+    }
 
+    inputItem.value = "";
+};
 
-    console.log(item);
+function checkbox(i) {
+    let checkboxItem = document.getElementById('checkbox-' + i);
+    
+    if(checkboxItem.classList.contains('checked')) {
+        checkboxItem.classList.remove('checked');
+    } else {
+        checkboxItem.classList.add('checked');
+    }
+}
+
+function deletar(i) {
+    let itemDaLista = document.getElementById('item-' + i);
+    itensNaLista.splice(i, 1);
+    listaDeCompras.removeChild(itemDaLista);
+}
+
+function editar(i) {
+    let nomeDoItem = document.getElementById('nome-item-' + i);
+    nomeDoItem.innerHTML = prompt('Digite um novo nome');
 }
